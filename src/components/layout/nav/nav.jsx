478305301433e7a256b0b5eb55b6c0c9a9ab2aa4
darthmorgot <dartmorgot@yandex.ru>
2024-01-25 22:54:44 +1000

    Добавил разметку в компонент.

--- src/components/layout/nav/nav.jsx
+++ src/components/layout/nav/nav.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 function Nav() {
-  return <></>;
+  return (
+    <nav className="nav">
+      <a href="#" className="nav__link button">Купить</a>
+      <a href="#" className="nav__link">Главная</a>
+    </nav>
+  );
 }
 
 export default Nav;
