37373c0e62a4b087ab60dc9cde6c59a1a57a2606
darthmorgot <dartmorgot@yandex.ru>
2024-01-25 22:59:27 +1000

    Немного изменил разметку компонента.

--- src/components/layout/header/header.jsx
+++ src/components/layout/header/header.jsx
@@ -1,15 +1,16 @@
-import React from "react";
-import Logo from "../../ui/logo/logo";
-import Nav from "../nav/nav";
+import React from 'react';
+import Logo from '../../ui/logo/logo';
+import Nav from '../nav/nav';
+import './style.scss';
 
 function Header() {
   return (
-    <>
-      <header className="header">
-        <Logo />
-        <Nav />
-      </header>
-    </>
+    <header className="header">
+      <div className="header__wrapper wrapper">
+        <Logo/>
+        <Nav/>
+      </div>
+    </header>
   );
 }
 
