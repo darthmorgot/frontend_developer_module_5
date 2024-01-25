b83aa963fa36b238ba976364696e327307712964
darthmorgot <dartmorgot@yandex.ru>
2024-01-25 22:51:18 +1000

    Добавил новый файл компонента.

---
+++ src/components/blocks/catalog-products/catalog-products.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function CatalogProducts() {
+  return (
+    <section className="catalog__products">
+      <h2 className="visually-hidden">Каталог товаров</h2>
+    </section>
+  );
+}
+
+export default CatalogProducts;
