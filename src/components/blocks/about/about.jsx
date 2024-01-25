3ec5c86d4655aa96a85f576089b4bc37615aaef1
darthmorgot <dartmorgot@yandex.ru>
2024-01-25 22:52:56 +1000

    Добавил разметку в компонент.

--- src/components/blocks/about/about.jsx
+++ src/components/blocks/about/about.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React from 'react';
+import './style.scss';
 
 function About() {
-  return <></>;
+  return (
+    <section className="about">
+      <div className="about__wrapper wrapper">
+        <div className="about__info">
+          <h1 className="about__title">Магазин фермерских продуктов с доставкой</h1>
+          <p className="about__description">Все продукты изготавливаются под заказ. Фермеры начинают готовить продукты за
+            день до отправки заказа клиентам. Именно поэтому мы принимаем заказы заранее и доставляем продукты максимально
+            свежими.</p>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default About;
