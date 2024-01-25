1bc23b43e1a48053dcbf39991aeaea122ac3a909
darthmorgot <dartmorgot@yandex.ru>
2024-01-25 22:54:39 +1000

    Добавил разметку в компонент.

--- src/components/pages/main-page/main-page.jsx
+++ src/components/pages/main-page/main-page.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import About from '../../blocks/about/about.jsx';
+import Advantages from '../../blocks/advantages/advantages.jsx';
 
 function MainPage() {
-  return <></>;
+  return (
+    <>
+      <About />
+      <Advantages />
+    </>
+  );
 }
 
 export default MainPage;
