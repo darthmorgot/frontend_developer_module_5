94ee6690c2783bb3edb8e509af9fce0db45571a9
darthmorgot <dartmorgot@yandex.ru>
2024-01-25 22:59:00 +1000

    Исправил импорт стилей.

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import PageWrapper from "/src/components/layout/page-wrapper/page-wrapper";
-import "./style.css";
+import "./style.scss";
 
 export default function App() {
   return <PageWrapper />;
