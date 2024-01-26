daa781e09bf4f9f3c8a37e31db1590f910168587
darthmorgot <dartmorgot@yandex.ru>
2024-01-27 00:57:23 +1000

    Добавил передачу пропсов в компонент.

--- src/components/pages/catalog-page/catalog-page.jsx
+++ src/components/pages/catalog-page/catalog-page.jsx
@@ -3,12 +3,12 @@ import OrderForm from '../../blocks/order-form/order-form.jsx';
 import CatalogProducts from '../../blocks/catalog-products/catalog-products.jsx';
 import './style.scss';
 
-function CatalogPage() {
+function CatalogPage({data}) {
   return (
     <div className="catalog wrapper">
       <h1 className="visually-hidden">Каталог продуктов</h1>
       <OrderForm />
-      <CatalogProducts />
+      <CatalogProducts data={data}/>
     </div>
   );
 }
