ed2aa78174e8d8210a9c2340350e05859c0dbe8a
darthmorgot <dartmorgot@yandex.ru>
2024-01-27 00:59:21 +1000

    Добавил импорт карточки продукта и поместил её в разметку.

--- src/components/blocks/catalog-products/catalog-products.jsx
+++ src/components/blocks/catalog-products/catalog-products.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import CardProduct from './card-product.jsx';
+import './style.scss';
 
-function CatalogProducts() {
+function CatalogProducts({data}) {
   return (
-    <section className="catalog__products">
+    <section className="catalog__products products">
       <h2 className="visually-hidden">Каталог товаров</h2>
+      <ul className="products__list products-list">
+        <CardProduct products={data}/>
+      </ul>
     </section>
   );
 }
