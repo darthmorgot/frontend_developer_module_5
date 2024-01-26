6e180d9240130946f55666bfbd88b499e9920956
darthmorgot <dartmorgot@yandex.ru>
2024-01-27 00:55:24 +1000

    Добавил импорт моковых данных, и передал их в качестве пропсов в компонент.

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import PageWrapper from "/src/components/layout/page-wrapper/page-wrapper";
 import "./style.scss";
+import products from '../../mocks/products.js';
 
 export default function App() {
-  return <PageWrapper />;
+  return <PageWrapper data={products}/>;
 }
