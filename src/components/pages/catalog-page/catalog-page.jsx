5700f7132b8ce97a94c261f4cf6b4b0a85d309dd
darthmorgot <dartmorgot@yandex.ru>
2024-01-29 01:28:09 +1000

    Добавил импорт функции setPageTitle, вызов этой функции.

--- src/components/pages/catalog-page/catalog-page.jsx
+++ src/components/pages/catalog-page/catalog-page.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 import OrderForm from '../../blocks/order-form/order-form.jsx';
 import CatalogProducts from '../../blocks/catalog-products/catalog-products.jsx';
 import './style.scss';
+import setPageTitle from '../../utils/utils.js';
 
 function CatalogPage({data}) {
+  setPageTitle('Каталог | Фермерский магазин');
+
   return (
     <div className="catalog wrapper">
       <h1 className="visually-hidden">Каталог продуктов</h1>
