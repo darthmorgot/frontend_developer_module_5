557d5521386cbfbca546c7b1b523743adb079a18
darthmorgot <dartmorgot@yandex.ru>
2024-01-29 01:29:33 +1000

    Добавил импорт функции setPageTitle, вызов этой функции, еще передачу пропса в компонент.

--- src/components/pages/main-page/main-page.jsx
+++ src/components/pages/main-page/main-page.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import About from '../../blocks/about/about.jsx';
 import Advantages from '../../blocks/advantages/advantages.jsx';
+import setPageTitle from '../../utils/utils.js';
 
-function MainPage() {
+function MainPage({setPage}) {
+  setPageTitle('Главная | Фермерский магазин');
+
   return (
     <>
       <About />
-      <Advantages />
+      <Advantages setPage={setPage}/>
     </>
   );
 }
