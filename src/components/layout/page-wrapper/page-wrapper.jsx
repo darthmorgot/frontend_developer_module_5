8400757c296406ad51107c588f4c551f16b3613c
darthmorgot <dartmorgot@yandex.ru>
2024-01-29 01:33:21 +1000

    Добавил стейт и передачу пропса в компонент, изменил подключение компонентов страниц.

--- src/components/layout/page-wrapper/page-wrapper.jsx
+++ src/components/layout/page-wrapper/page-wrapper.jsx
@@ -1,16 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 import Header from "../header/header";
 import Footer from "../footer/footer";
 import MainPage from "../../pages/main-page/main-page";
 import CatalogPage from '../../pages/catalog-page/catalog-page.jsx';
 
 function PageWrapper({data}) {
+  const [page, setPage] = useState('main-page');
+
+  function handlePage(e) {
+    setPage(e.target.dataset.page);
+  }
+
   return (
     <>
-      <Header />
+      <Header page={page} setPage={handlePage}/>
       <main className="main main-container">
-        {/*<MainPage />*/}
-        <CatalogPage data={data}/>
+        {page === 'main-page' ? <MainPage setPage={handlePage} /> : <CatalogPage data={data}/>}
       </main>
       <Footer />
     </>
