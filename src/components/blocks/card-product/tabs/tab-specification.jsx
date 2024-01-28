e55b9e0c8025b889f8f5bcec2be7cb28f5e7808f
darthmorgot <dartmorgot@yandex.ru>
2024-01-29 01:38:18 +1000

    Создал новый файл компонента для вкладки на карточке товара.

---
+++ src/components/blocks/card-product/tabs/tab-specification.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function TabSpecification({activeTab, item}) {
+  return activeTab === 'tab_2'
+    ? <dl>
+      <div>
+        <dt>Масса:</dt>
+        <dd>{item.spec.weight}</dd>
+      </div>
+      <div>
+        <dt>Срок годности:</dt>
+        <dd>{item.spec.expiration_date}</dd>
+      </div>
+      <div>
+        <dt>Порода:</dt>
+        <dd>{item.spec.breed}</dd>
+      </div>
+      <div>
+        <dt>Место происхождения:</dt>
+        <dd>{item.spec.place_origin}</dd>
+      </div>
+    </dl>
+    : null;
+}
+
+export default TabSpecification;
