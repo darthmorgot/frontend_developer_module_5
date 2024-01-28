0951c71730b3962147ec3fb871c6e77f6f8aebcb
darthmorgot <dartmorgot@yandex.ru>
2024-01-29 01:38:13 +1000

    Создал новый файл компонента для вкладки на карточке товара.

---
+++ src/components/blocks/card-product/tabs/tab-feature.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function TabFeature({activeTab, item}) {
+  return activeTab === 'tab_3'
+    ? <dl>
+      <div>
+        <dt>Энергетическая ценность:</dt>
+        <dd>{item.features.energy_value}</dd>
+      </div>
+      <div>
+        <dt>Пищевая ценность:</dt>
+        <dd>{item.features.nutritional_value}</dd>
+      </div>
+    </dl>
+    : null;
+}
+
+export default TabFeature;
