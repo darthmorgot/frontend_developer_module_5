aae8f726fb554502b5e8d3a0595f8dddfa37e654
darthmorgot <dartmorgot@yandex.ru>
2024-01-29 01:28:41 +1000

    Добавил передачу пропса в компонент.

--- src/components/layout/header/header.jsx
+++ src/components/layout/header/header.jsx
@@ -3,12 +3,12 @@ import Logo from '../../ui/logo/logo';
 import Nav from '../nav/nav';
 import './style.scss';
 
-function Header() {
+function Header({page, setPage}) {
   return (
     <header className="header">
       <div className="header__wrapper wrapper">
         <Logo/>
-        <Nav/>
+        <Nav page={page} setPage={setPage}/>
       </div>
     </header>
   );
