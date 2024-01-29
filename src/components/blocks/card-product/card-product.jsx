2b9f398742aba6ba812bb19c6d541c635a16061b
darthmorgot <dartmorgot@yandex.ru>
2024-01-30 00:16:42 +1000

    Добавил импорт компонента Title, его подключение в коде, добавил проверку для данных из пропсов.

--- src/components/blocks/card-product/card-product.jsx
+++ src/components/blocks/card-product/card-product.jsx
@@ -1,20 +1,19 @@
 import React from 'react';
+import Title, {TitleSize, Heading} from '../../ui/title/title.jsx';
 import TabsData from './tabs/tabs-data.jsx';
 import './style.scss';
 
 function CardProduct({products}) {
-  return (
-    products.map(item => (
+  return products && products.length ? products.map(item => (
       <li className="products-list__item" key={item.id}>
         <div className="products__card card">
-          <h2 className="card__title">{item.title}</h2>
+          <Title heading={Heading.H2} size={TitleSize.MEDIUM}>{item.title}</Title>
           <img src={item.image} width="248" height="248" alt="Картинка товара"
                className="card__image"/>
           <TabsData item={item}/>
         </div>
       </li>
-    ))
-  );
+    )) : null;
 }
 
 export default CardProduct;
