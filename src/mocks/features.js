64680fb3f4d99af3cb83ceb091f32177f94d0a67
darthmorgot <dartmorgot@yandex.ru>
2024-01-30 00:18:04 +1000

    Создал новый файл с моковыми данными.

---
+++ src/mocks/features.js
@@ -0,0 +1,36 @@
+const features = [
+  {
+    id: 0,
+    category: 'Фермерские продукты',
+    title: 'Еда намного вкуснее',
+    description: 'Домашняя колбаса из мяса, соли и специй и колбаса <br/> из магазина — два настолько разных продукта, <br/> что они даже не родственники',
+    icon: '/src/assets/img/icon-eat.svg',
+    isNegative: false
+  },
+  {
+    id: 1,
+    category: 'Магазинные продукты',
+    title: 'Просроченные продукты',
+    description: 'Из-за большого количество товара сотрудники магазинов не успевают своевременно производить замену товара',
+    icon: '/src/assets/img/icon-garbage.svg',
+    isNegative: true
+  },
+  {
+    id: 2,
+    category: 'Фермерские продукты',
+    title: 'Натуральные продукты',
+    description: 'Поставляем местные органические продукты выращенные без пестицидов и химических удобрений.',
+    icon: '/src/assets/img/icon-sprout.svg',
+    isNegative: false
+  },
+  {
+    id: 3,
+    category: 'Магазинные продукты',
+    title: 'Некачественное мясо',
+    description: 'Мясные полуфабрикаты, в которых содержится чрезмерно много натрия, вредных жиров, консервантов',
+    icon: '/src/assets/img/icon-no-meat.svg',
+    isNegative: true
+  },
+];
+
+export default features;
