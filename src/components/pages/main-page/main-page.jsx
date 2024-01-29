03d9ac638a6300c896e9a5f79ee7b7f08de603ce
darthmorgot <dartmorgot@yandex.ru>
2024-01-30 00:18:52 +1000

    Добавил еще данные в пропсы, и мелкие правки кода.

--- src/components/pages/main-page/main-page.jsx
+++ src/components/pages/main-page/main-page.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import About from '../../blocks/about/about.jsx';
-import Advantages from '../../blocks/advantages/advantages.jsx';
+import AdvantagesList from '../../blocks/advantages-list/advantages-list.jsx';
 import setPageTitle from '../../utils/utils.js';
 
-function MainPage({setPage}) {
+function MainPage({setPage, features}) {
   setPageTitle('Главная | Фермерский магазин');
 
   return (
     <>
       <About />
-      <Advantages setPage={setPage}/>
+      <AdvantagesList setPage={setPage} features={features}/>
     </>
   );
 }
