e49534a468093abc70e8b94acd3a00294a45058c
darthmorgot <dartmorgot@yandex.ru>
2024-01-30 17:49:08 +1000

    Мелкие правки кода.

--- src/components/ui/card-product/tabs/tab-description.jsx
+++ src/components/ui/card-product/tabs/tab-description.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function TabDescription({activeTab, item}) {
   return activeTab === 'tab_0' ? <>
-      <p>{item.about}</p>
+      <p dangerouslySetInnerHTML={{__html: item.about}}/>
       <p>{item.price} руб. / {item.weight} гр.</p>
     </>
     : null;
