efd63124f2eac64c3cd523fdb692afebacdd4eaf
darthmorgot <dartmorgot@yandex.ru>
2024-02-01 01:03:41 +1000

    Мелкие правки кода.

--- src/components/layout/nav/nav.jsx
+++ src/components/layout/nav/nav.jsx
@@ -6,7 +6,7 @@ function Nav({page, setPage}) {
     <nav className="nav">
       {
         page === 'main-page'
-          ? <Button classButton={'nav__link'} dataButton={'catalog-page'} handleButton={setPage}>Купить</Button>
+          ? <Button classButton={'nav__link'} link dataButton={'catalog-page'} handleButton={setPage}>Купить</Button>
           : <a className="nav__link nav__link--home" data-page="main-page" onClick={setPage} tabIndex="0">Главная</a>
       }
     </nav>
