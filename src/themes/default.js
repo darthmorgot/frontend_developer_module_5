66abc7260a1bca63872b5e276d2d13405fd26262
darthmorgot <dartmorgot@yandex.ru>
2024-02-01 01:02:03 +1000

    Создал файл с дефолтной темой.

---
+++ src/themes/default.js
@@ -0,0 +1,13 @@
+export const defaultTheme = {
+  white: '#ffffff',
+  black: '#000000',
+  mineShaft: '#333333',
+  wildSand: '#f6f6f6',
+  pattensBlue: '#d8ecfe',
+  chromeWhite: '#e1edce',
+  chelseaCucumber: '#88aa4d',
+  linen: '#f8ddd7',
+  flamingo: '#f75531',
+  seaBuckthorn: '#fc9b27',
+  crusta: '#fc7427',
+};
