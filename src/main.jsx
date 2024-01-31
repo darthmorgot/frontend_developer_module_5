0fd0f9e859b18b74f0f6fdf45a362febd6dc242a
darthmorgot <dartmorgot@yandex.ru>
2024-02-01 01:03:18 +1000

    Добавил импорт ThemeProvider и дефолтной темы, подключил их в разметке.

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {ThemeProvider} from 'styled-components';
 import App from './components/app/app.jsx';
+import {defaultTheme} from './themes/default.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={defaultTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
