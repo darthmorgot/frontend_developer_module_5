3a6444f896ec6582bfd384661a91f97b35d8daec
darthmorgot <dartmorgot@yandex.ru>
2024-02-01 01:00:05 +1000

    Добавил импорт глобальных стилей и подключил их в разметке.

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import PageWrapper from "/src/components/layout/page-wrapper/page-wrapper";
-import "./style.scss";
+import {GlobalStyle} from './style.js';
 import products from '../../mocks/products.js';
 import features from '../../mocks/features.js';
 
 export default function App() {
-  return <PageWrapper products={products} features={features}/>;
+  return (
+    <>
+      <GlobalStyle />
+      <PageWrapper products={products} features={features}/>
+    </>
+  );
 }
