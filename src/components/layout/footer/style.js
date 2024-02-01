618a4e3d879d3a451f9a1aec17ad39d17c3507e2
darthmorgot <dartmorgot@yandex.ru>
2024-02-01 23:41:53 +1000

    Создал новый файл стилизованный компонент для футера.

---
+++ src/components/layout/footer/style.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const StyledFooter = styled.footer`
+  border-top: 1px solid rgba(0 0 0 / 0.1);
+`;
+
+export const FooterWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding-left: 16px;
+`;
+
+export const FooterCopyright = styled.p``;
