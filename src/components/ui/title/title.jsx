d0e812bc00e985d11c15ae61dffaf28321b8206c
darthmorgot <dartmorgot@yandex.ru>
2024-02-01 23:44:51 +1000

    Изменил разметку с применением стилизованных компонентов.

--- src/components/ui/title/title.jsx
+++ src/components/ui/title/title.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import './style.scss';
+import {StyledTitle} from './style.js';
 
-export const Heading = {
+export const TitleLevel = {
   H1: 'h1',
   H2: 'h2',
   H3: 'h3'
@@ -10,19 +10,13 @@ export const Heading = {
 export const TitleSize = {
   BIG: 'big',
   MEDIUM: 'medium',
-  SMALL: 'small',
-  EXTRA_SMALL: 'extra-small'
+  SMALL: 'small'
 };
 
-function Title({heading, size, children}) {
-  switch (heading) {
-    case 'h1':
-      return <h1 className={`title${size ? ` title--${size}` : ""}`}>{children}</h1>
-    case 'h2':
-      return <h2 className={`title${size ? ` title--${size}` : ""}`}>{children}</h2>
-    case 'h3':
-      return <h3 className={`title${size ? ` title--${size}` : ""}`}>{children}</h3>
-  }
+function Title({level, size, children}) {
+  return (
+    <StyledTitle as={level} $size={size}>{children}</StyledTitle>
+  );
 }
 
 export default Title;
