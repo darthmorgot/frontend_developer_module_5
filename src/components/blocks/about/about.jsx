8ccb32f80aa95dbf91425235f1b9e7227b3c4bc8
darthmorgot <dartmorgot@yandex.ru>
2024-02-01 23:35:26 +1000

    Изменил разметку с применением стилизованных компонентов.

--- src/components/blocks/about/about.jsx
+++ src/components/blocks/about/about.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import Title, {TitleSize, Heading} from '../../ui/title/title.jsx';
-import './style.scss';
+import Title, {TitleSize, TitleLevel} from '../../ui/title/title.jsx';
+import {Section, AboutWrapper, TextWrapper, Description} from './style.js';
 
 function About() {
   return (
-    <section className="about">
-      <div className="about__wrapper wrapper">
-        <div className="about__info">
-          <Title heading={Heading.H1} size={TitleSize.BIG}>Магазин фермерских продуктов с доставкой</Title>
-          <p className="about__description">Все продукты изготавливаются под заказ. Фермеры начинают готовить продукты за
+    <Section>
+      <AboutWrapper className="wrapper">
+        <TextWrapper>
+          <Title level={TitleLevel.H1} size={TitleSize.BIG}>Магазин фермерских продуктов с доставкой</Title>
+          <Description>Все продукты изготавливаются под заказ. Фермеры начинают готовить продукты за
             день до отправки заказа клиентам. Именно поэтому мы принимаем заказы заранее и доставляем продукты максимально
-            свежими.</p>
-        </div>
-      </div>
-    </section>
+            свежими.</Description>
+        </TextWrapper>
+      </AboutWrapper>
+    </Section>
   );
 }
 
