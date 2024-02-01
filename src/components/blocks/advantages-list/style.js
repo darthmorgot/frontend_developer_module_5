8670cad3d294c720ba5ca7af26369d1ba0bd308a
darthmorgot <dartmorgot@yandex.ru>
2024-02-01 23:41:38 +1000

    Создал новый файл стилизованный компонент для блока advantages-list.

---
+++ src/components/blocks/advantages-list/style.js
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+
+export const Section = styled.section`
+  padding-top: 101px;
+  padding-bottom: 108px;
+`;
+
+export const Slogan = styled.span`
+  display: block;
+  margin-bottom: 62px;
+  text-align: center;
+  font-size: 36px;
+  font-weight: 700;
+  line-height: 115%;
+`;
+
+export const List = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  margin: 0 0 44px;
+  padding: 0;
+  list-style: none;
+`;
+
+export const ListItem = styled.li`
+  width: 540px;
+  min-height: 197px;
+  margin-bottom: 20px;
+
+  &:first-child div {
+    margin-left: 72px;
+  }
+
+  &:nth-child(3) section {
+    background-position: 20px 20px;
+  }
+`;
