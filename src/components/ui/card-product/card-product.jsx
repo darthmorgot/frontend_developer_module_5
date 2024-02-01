d35bb98addc58bba71c151dc0be514954eee981b
darthmorgot <dartmorgot@yandex.ru>
2024-02-01 23:36:38 +1000

    Мелкие правки кода.

--- src/components/ui/card-product/card-product.jsx
+++ src/components/ui/card-product/card-product.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Title, {TitleSize, Heading} from '../title/title.jsx';
+import Title, {TitleSize, TitleLevel} from '../title/title.jsx';
 import TabsData from './tabs/tabs-data.jsx';
 import './style.scss';
 
@@ -7,7 +7,7 @@ function CardProduct({products}) {
   return products && products.length ? products.map(item => (
       <li className="products-list__item" key={item.id}>
         <div className="products__card card">
-          <Title heading={Heading.H2} size={TitleSize.MEDIUM}>{item.title}</Title>
+          <Title level={TitleLevel.H2} size={TitleSize.MEDIUM}>{item.title}</Title>
           <img src={item.image} width="248" height="248" alt="Картинка товара"
                className="card__image"/>
           <TabsData item={item}/>
