e1b61bf889e8c34a8c6e0d6876a7978e2d45d2cb
darthmorgot <dartmorgot@yandex.ru>
2024-02-02 19:14:24 +1000

    Создал новый файл стилизованный компонент для карточки товара.

---
+++ src/components/ui/card-product/tabs/style.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const StyledTabs = styled.div`
+  grid-column: 2 / 3;
+  grid-row: 2 / 3;
+`;
+
+export const TabsWrapper = styled.div``;
+
+export const TabsItem = styled.div`
+  font-size: 14px;
+  line-height: 150%;
+`;
