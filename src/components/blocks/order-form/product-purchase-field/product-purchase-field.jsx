9858d682cd5805ca03d8eb1d13afa0e926322188
darthmorgot <dartmorgot@yandex.ru>
2024-02-02 19:05:26 +1000

    Изменил разметку с применением стилизованных компонентов.

---
+++ src/components/blocks/order-form/product-purchase-field/product-purchase-field.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Title, {TitleSize, TitleLevel} from '../../../ui/title/title.jsx';
+import {OrderFieldset, Address, Label, HiddenLabelText, Input, ShowPrice, SpanPrice, Output, SubmitButton} from './style.js';
+
+// Блок формы с адресом, ценой и кнопкой покупки на странице каталога
+function ProductPurchaseField({prices}) {
+  return (
+    <OrderFieldset>
+      <Title level={TitleLevel.H3} size={TitleSize.SMALL}>Сделать заказ</Title>
+      <Address>
+        <Label>
+          <HiddenLabelText>Введите адрес доставки</HiddenLabelText>
+          <Input/>
+        </Label>
+      </Address>
+      <ShowPrice>
+        <SpanPrice>Цена</SpanPrice>
+        <Output>1 200 руб.</Output>
+      </ShowPrice>
+      <SubmitButton as={'button'}>Купить</SubmitButton>
+    </OrderFieldset>
+  );
+}
+
+export default ProductPurchaseField;
