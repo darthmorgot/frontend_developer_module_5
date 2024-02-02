42ced2a22bf2a443aff44fda61c30d63cd4d5166
darthmorgot <dartmorgot@yandex.ru>
2024-02-02 19:12:08 +1000

    Создал новый файл стилизованный компонент для блока формы с оформлением покупки.

---
+++ src/components/blocks/order-form/product-purchase-field/style.js
@@ -0,0 +1,62 @@
+import styled from 'styled-components';
+import {StyleButton} from '../../../ui/button/style.js';
+
+export const OrderFieldset = styled.fieldset`
+  padding: 23px 20px 20px 19px;
+  margin: 0 0 17px;
+  border: 1px solid rgba(0 0 0 / 0.10);
+  box-shadow: 0 0 1px 0 rgba(0 0 0 / 0.04), 0 2px 6px 0 rgba(0 0 0 / 0.04), 0 10px 20px 0 rgba(0 0 0 / 0.04);
+
+  h3 {
+    margin-bottom: 24px;
+  }
+`;
+
+export const Address = styled.div`
+  margin-bottom: 17px;
+`;
+
+export const Label = styled.label`
+  display: block;
+`;
+
+export const HiddenLabelText = styled.span.attrs({className: 'visually-hidden'})``;
+
+export const Input = styled.input.attrs({
+  type: 'text',
+  name: 'address',
+  placeholder: 'Введите адрес доставки'
+})`
+  width: 100%;
+  padding-top: 16px;
+  padding-left: 12px;
+  padding-bottom: 14px;
+  font-size: 14px;
+  border: 1px solid rgba(0 0 0 / 0.10);
+  background: ${props => props.theme.wildSand};
+
+  &:focus {
+    outline: 1px solid ${props => props.theme.crusta};
+    outline-offset: 2px;
+  }
+`;
+
+export const ShowPrice = styled.div``;
+
+export const SpanPrice = styled.span`
+  display: block;
+  margin: 0 0 5px;
+  font-size: 14px;
+`;
+
+export const Output = styled.output`
+  display: block;
+  margin-bottom: 32px;
+  font-size: 24px;
+  font-weight: 700;
+  line-height: 130%;
+`;
+
+export const SubmitButton = styled(StyleButton).attrs({type: 'submit'})`
+  width: 314px;
+`;
