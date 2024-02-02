13111b2ff11e7e6ca50e0fbd0a36c40b135751cf
darthmorgot <dartmorgot@yandex.ru>
2024-02-02 19:03:19 +1000

    Изменил разметку с применением стилизованных компонентов.

--- src/components/pages/catalog-page/catalog-page.jsx
+++ src/components/pages/catalog-page/catalog-page.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import OrderForm from '../../blocks/order-form/order-form.jsx';
 import CatalogProducts from '../../blocks/catalog-products/catalog-products.jsx';
-import './style.scss';
+import {Catalog} from './style.js';
+import HiddenTitle from '../../ui/hidden-title/hidden-title.jsx';
 import setPageTitle from '../../utils/utils.js';
 
 function CatalogPage({products}) {
   setPageTitle('Каталог | Фермерский магазин');
 
   return (
-    <div className="catalog wrapper">
-      <h1 className="visually-hidden">Каталог продуктов</h1>
+    <Catalog className="wrapper">
+      <HiddenTitle level>Каталог продуктов</HiddenTitle>
       <OrderForm />
       <CatalogProducts products={products}/>
-    </div>
+    </Catalog>
   );
 }
 
