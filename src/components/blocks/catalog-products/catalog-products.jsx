45b4d210de63e312f69d0f3fb9bde6eae0c057ae
darthmorgot <dartmorgot@yandex.ru>
2024-02-07 00:18:39 +1000

    Добавил описание, импорт модулей и стилей для swiper, добавил в ProductList параметры для настройки слайдера, добавил импорт ListItem и применил его в разметке.

--- src/components/blocks/catalog-products/catalog-products.jsx
+++ src/components/blocks/catalog-products/catalog-products.jsx
@@ -1,15 +1,37 @@
 import React from 'react';
 import CardProduct from '../../ui/card-product/card-product.jsx';
 import HiddenTitle from '../../ui/hidden-title/hidden-title.jsx';
-import {Section, ProductList} from './style.js';
+import {Scrollbar, Mousewheel} from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/scrollbar';
+import {Section, ProductList, ListItem} from './style.js';
 
-// Блок с товарами на странице каталога
+/**
+ * Компонент списка с товарами на странице каталога
+ * @param products props Данные о товарах поступают из CatalogPage
+ * @returns {JSX.Element} Разметка списка
+ * @constructor
+ */
 function CatalogProducts({products}) {
   return (
     <Section>
       <HiddenTitle>Каталог товаров</HiddenTitle>
-      <ProductList>
-        <CardProduct products={products}/>
+      <ProductList
+        direction={'vertical'}
+        slidesPerView={'auto'}
+        spaceBetween={20}
+        grabCursor={true}
+        scrollbar={{
+          hide: false,
+        }}
+        mousewheel={true}
+        modules={[Mousewheel, Scrollbar]}
+      >
+        {products && products.length ? products.map(item => (
+          <ListItem key={item.id}>
+            <CardProduct product={item}/>
+          </ListItem>
+        )) : null}
       </ProductList>
     </Section>
   );
