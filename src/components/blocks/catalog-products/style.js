f87f020528ae27ddc9dcaa64db583e531603c129
darthmorgot <dartmorgot@yandex.ru>
2024-02-07 00:24:53 +1000

    Добавил стилизованный компонент ListItem, стили в компоненты.

--- src/components/blocks/catalog-products/style.js
+++ src/components/blocks/catalog-products/style.js
@@ -1,9 +1,18 @@
 import styled from 'styled-components';
+import {Swiper} from 'swiper/react';
+import {SwiperSlide} from 'swiper/react';
 
 export const Section = styled.section``;
 
-export const ProductList = styled.ul`
+export const ProductList = styled(Swiper)`
+  height: 735px;
   margin: 0;
   padding: 0;
   list-style: none;
+`;
+
+export const ListItem = styled(SwiperSlide)`
+  width: 727px;
+  height: 288px;
+  margin-bottom: 12px;
 `;
