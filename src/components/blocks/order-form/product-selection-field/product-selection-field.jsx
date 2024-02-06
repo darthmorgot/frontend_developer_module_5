c7255ba75cb057348c49b3f3cbaf17f86150970b
darthmorgot <dartmorgot@yandex.ru>
2024-02-07 00:23:44 +1000

    Добавил описание, удалил переменную createList, код из этой переменной применил в разметке.

--- src/components/blocks/order-form/product-selection-field/product-selection-field.jsx
+++ src/components/blocks/order-form/product-selection-field/product-selection-field.jsx
@@ -1,29 +1,41 @@
 import React from 'react';
 import Title, {TitleSize, TitleLevel} from '../../../ui/title/title.jsx';
-import {SelectFieldset, SelectList, ListItem, Label, SpanName, Input, SpanMark} from './style.js';
+import {
+  SelectFieldset,
+  SelectList,
+  ListItem,
+  Label,
+  SpanName,
+  Input,
+  SpanMark
+} from './style.js';
 
-// Блок формы для выбора товара из списка на странице каталога
+/**
+ * Компонент поля формы для выбора товара из списка на странице каталога
+ * @param goods props Данные продукта поступают из OrderForm
+ * @param changed props Коллбэк поступает из OrderForm
+ * @returns {JSX.Element} Разметка поля формы
+ * @constructor
+ */
 function ProductSelectionField({goods, changed}) {
-  const createList = goods && goods.length ? goods.map(item => (
-      <ListItem key={item.id}>
-        <Label>
-          <SpanName>{item.content}</SpanName>
-          <Input className='visually-hidden'
-                 name={item.name}
-                 checked={item.checked}
-                 onChange={e => changed(item.id, e)}
-          />
-          <SpanMark></SpanMark>
-        </Label>
-      </ListItem>
-    )
-  ) : null;
-
   return (
     <SelectFieldset>
       <Title level={TitleLevel.H3} size={TitleSize.SMALL}>Выберите продукты</Title>
       <SelectList>
-        {createList}
+        {goods && goods.length ? goods.map(item => (
+            <ListItem key={item.id}>
+              <Label>
+                <SpanName>{item.title}</SpanName>
+                <Input className='visually-hidden'
+                       name={item.name}
+                       checked={item.checked}
+                       onChange={e => changed(item.id, e)}
+                />
+                <SpanMark></SpanMark>
+              </Label>
+            </ListItem>
+          )
+        ) : null}
       </SelectList>
     </SelectFieldset>
   );
