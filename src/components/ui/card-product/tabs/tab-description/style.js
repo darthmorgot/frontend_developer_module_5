bbab4788a1271363c380e16b898cb9146a025cb1
darthmorgot <dartmorgot@yandex.ru>
2024-02-08 00:42:50 +1000

    Добавил стили для кнопки для открытия/скрытия описания на карточке товара.

--- src/components/ui/card-product/tabs/tab-description/style.js
+++ src/components/ui/card-product/tabs/tab-description/style.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import {StyleButton} from '../../../button/style.js';
 
 const Paragraph = styled.p`
   margin: 0;
@@ -14,4 +15,34 @@ export const Price = styled(Paragraph)`
   background-color: ${props => props.theme.pattensBlue};
   font-size: 14px;
   font-weight: 700;
+`;
+
+export const WrapperPriceDescription = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const DescriptionButton = styled(StyleButton)`
+  width: auto;
+  padding: 0;
+  margin: 0;
+  color: ${props => props.theme.mineShaft};
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 150%;
+  background-color: transparent;
+
+  &:hover {
+    background: transparent;
+    text-decoration: underline;
+  }
+
+  &:active {
+    box-shadow: none;
+  }
+
+  &:focus {
+    outline: 1px solid ${props => props.theme.crusta};
+    outline-offset: 2px;
+  }
 `;
