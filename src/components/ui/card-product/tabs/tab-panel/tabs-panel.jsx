386904f14c84346f24dccd42838e1f916f1e78fb
darthmorgot <dartmorgot@yandex.ru>
2024-02-08 00:43:41 +1000

    Добавил описание, удалил переменную createButtonPanel, код из этой переменной применил в разметке.

--- src/components/ui/card-product/tabs/tab-panel/tabs-panel.jsx
+++ src/components/ui/card-product/tabs/tab-panel/tabs-panel.jsx
@@ -1,6 +1,13 @@
 import {useState} from 'react';
 import {StyledTabsPanel, ButtonTabsPanel} from './style.js';
 
+/**
+ * Компонент панели с кнопками вкладок на карточке товара
+ * @param activeTab props Данные об активной вкладке посупают из Tabs
+ * @param handleClick props Коллбэк поступает из Tabs
+ * @returns {JSX.Element} Разметка панели
+ * @constructor
+ */
 function TabsPanel({activeTab, handleClick}) {
   const [buttons, setButton] = useState([
     {id: 'tab_0', name: 'Описание'},
@@ -8,19 +15,17 @@ function TabsPanel({activeTab, handleClick}) {
     {id: 'tab_2', name: 'Свойства'}
   ]);
 
-  const createButtonPanel = buttons.map(button => (
-    <ButtonTabsPanel
-      key={button.id}
-      id={button.id}
-      type='button'
-      className={activeTab === button.id ? 'active' : null}
-      onClick={handleClick}
-    >{button.name}</ButtonTabsPanel>
-  ));
-
   return (
     <StyledTabsPanel>
-      {createButtonPanel}
+      {buttons.map(button => (
+        <ButtonTabsPanel
+          key={button.id}
+          id={button.id}
+          type='button'
+          className={activeTab === button.id ? 'active' : null}
+          onClick={handleClick}
+        >{button.name}</ButtonTabsPanel>
+      ))}
     </StyledTabsPanel>
   );
 }
