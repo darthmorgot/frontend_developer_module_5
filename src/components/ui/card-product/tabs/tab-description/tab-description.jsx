30ed8ef32fc94485449de8aa56ab936b9b1c1ef1
darthmorgot <dartmorgot@yandex.ru>
2024-02-09 01:40:58 +1000

    Добавил описание, стейт, необходимые переменные, обработчик для стейта, импорт стилизованного компонента кнопки для открытия-скрытия текста описания продукта.

--- src/components/ui/card-product/tabs/tab-description/tab-description.jsx
+++ src/components/ui/card-product/tabs/tab-description/tab-description.jsx
@@ -1,10 +1,29 @@
-import React from 'react';
-import {Text, Price} from './style.js';
+import {useState} from 'react';
+import {Text, WrapperPriceDescription, DescriptionButton, Price} from './style.js';
 
+/**
+ * Компонент блока с описанием на карточке товара
+ * @param activeTab props Данные об активной вкладке поступают из Tabs
+ * @param item props Данные о продукте поступают из Tabs
+ * @returns {JSX.Element|null} Разметка блока
+ * @constructor
+ */
 function TabDescription({activeTab, item}) {
+  const MAX_TEXT_SIZE = 250;
+  const [isShowDescription, setShowDescription] = useState(false);
+  const text = isShowDescription ? item.about : item.about.slice(0, MAX_TEXT_SIZE);
+  const buttonText = item.about.length > MAX_TEXT_SIZE ? (!isShowDescription ? 'Подробнее' : 'Скрыть') : null;
+
+  const handleShowDescription = () => {
+    setShowDescription(!isShowDescription);
+  };
+
   return activeTab === 'tab_0' ? <>
-      <Text dangerouslySetInnerHTML={{__html: item.about}}/>
-      <Price>{item.price} руб. / {item.weight} гр.</Price>
+      <Text dangerouslySetInnerHTML={{__html: text}}/>
+      <WrapperPriceDescription>
+        <Price>{item.price} руб. / {item.weight} гр.</Price>
+        <DescriptionButton as={'button'} onClick={handleShowDescription}>{buttonText}</DescriptionButton>
+      </WrapperPriceDescription>
     </>
     : null;
 }
