d866a409b9015635a4b7ae41eead0dcfaeff6e80
darthmorgot <dartmorgot@yandex.ru>
2024-02-09 01:47:22 +1000

    Добавил описание, и условие для вывода строки с названием породы.

--- src/components/ui/card-product/tabs/tab-specification/tab-specification.jsx
+++ src/components/ui/card-product/tabs/tab-specification/tab-specification.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import {Dl, RowWrapper, Dt, Dd} from './style.js';
 
+/**
+ * Компонент вкладки с характеристиками товара
+ * @param activeTab props Данные об активной вкладке поступают из Tabs
+ * @param item props Данные о товаре поступают из Tabs
+ * @returns {JSX.Element|null} Разметка вкладки
+ * @constructor
+ */
 function TabSpecification({activeTab, item}) {
   return activeTab === 'tab_1'
     ? <Dl>
@@ -12,10 +19,14 @@ function TabSpecification({activeTab, item}) {
         <Dt>Срок годности:</Dt>
         <Dd>{item.spec.expiration_date}</Dd>
       </RowWrapper>
-      <RowWrapper>
-        <Dt>Порода:</Dt>
-        <Dd>{item.spec.breed}</Dd>
-      </RowWrapper>
+      {
+        item.spec.breed
+        ? <RowWrapper>
+            <Dt>Порода:</Dt>
+            <Dd>{item.spec.breed}</Dd>
+          </RowWrapper>
+        : null
+      }
       <RowWrapper>
         <Dt>Место происхождения:</Dt>
         <Dd>{item.spec.place_origin}</Dd>
