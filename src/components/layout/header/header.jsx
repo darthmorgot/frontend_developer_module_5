2bf7563b1821ee1123a0c7395e8fdb81466cec81
darthmorgot <dartmorgot@yandex.ru>
2024-02-10 00:53:45 +1000

    Добавил описание.

--- src/components/layout/header/header.jsx
+++ src/components/layout/header/header.jsx
@@ -3,6 +3,13 @@ import Logo from '../../ui/logo/logo';
 import Nav from '../nav/nav';
 import {StyledHeader, HeaderWrapper} from './style.js';
 
+/**
+ * Компонент хедера на странице сайта
+ * @param page props Данные о странице сайта поступает из PageWrapper
+ * @param setPage props Коллбэк поступает из PageWrapper
+ * @returns {JSX.Element} Разметка хедера
+ * @constructor
+ */
 function Header({page, setPage}) {
   return (
     <StyledHeader>
