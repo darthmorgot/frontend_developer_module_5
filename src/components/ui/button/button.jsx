f2cc3d6f2477c8fcfdf92ecf7d9a0062f96977f0
darthmorgot <dartmorgot@yandex.ru>
2024-02-10 00:53:29 +1000

    Добавил описание.

--- src/components/ui/button/button.jsx
+++ src/components/ui/button/button.jsx
@@ -1,6 +1,17 @@
 import React from 'react';
 import {StyleButton} from './style.js';
 
+/**
+ * Компонент кнопки
+ * @param classButton props Данные о классе поступают на месте применения
+ * @param link props Данные о ссылке поступают на месте применения
+ * @param maxWidth props Данные о размере поступают на месте применения
+ * @param dataButton props Данные о странице поступают на месте применения
+ * @param handleButton props Коллбэк поступает на месте применения
+ * @param children props Данные о контенте поступают на месте применения
+ * @returns {JSX.Element} Разметка кнопки
+ * @constructor
+ */
 function Button({classButton, link, maxWidth, dataButton, handleButton, children}) {
   return (
     <StyleButton
