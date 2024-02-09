9c94af3b66ae1e45ff63ca75c13f3072baa8adf6
darthmorgot <dartmorgot@yandex.ru>
2024-02-10 00:53:39 +1000

    Добавил описание.

--- src/components/layout/footer/footer.jsx
+++ src/components/layout/footer/footer.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import Logo from '../../ui/logo/logo.jsx';
 import {StyledFooter, FooterWrapper, FooterCopyright} from './style.js';
 
+/**
+ * Компонент футера на странице сайта
+ * @returns {JSX.Element} Разметка футера
+ * @constructor
+ */
 function Footer() {
   return (
     <StyledFooter>
