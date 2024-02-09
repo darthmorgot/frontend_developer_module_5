2dba5cdea27302ae84e1a336bfd52c844b0054bb
darthmorgot <dartmorgot@yandex.ru>
2024-02-10 00:54:43 +1000

    Добавил описание.

--- src/components/ui/title/title.jsx
+++ src/components/ui/title/title.jsx
@@ -13,6 +13,14 @@ export const TitleSize = {
   SMALL: 'small'
 };
 
+/**
+ * Компонент заголовка
+ * @param level props Данные об уровне заголовка поступают на месте применения
+ * @param size props Данные о величине шрифта поступают на месте применения
+ * @param children props Данные о контенте поступают на месте применения
+ * @returns {JSX.Element|null} Разметка заголовка
+ * @constructor
+ */
 function Title({level, size, children}) {
   return (
     <StyledTitle as={level} $size={size}>{children}</StyledTitle>
