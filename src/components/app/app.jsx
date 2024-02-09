10278ad7515462b093ed52d083c6297c55ced9da
darthmorgot <dartmorgot@yandex.ru>
2024-02-10 00:53:21 +1000

    Добавил описание.

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -4,6 +4,11 @@ import {GlobalStyle} from './style.js';
 import products from '../../mocks/products.js';
 import features from '../../mocks/features.js';
 
+/**
+ * Основной компонент проекта
+ * @returns {JSX.Element} Разметка страниц проекта
+ * @constructor
+ */
 export default function App() {
   return (
     <>
