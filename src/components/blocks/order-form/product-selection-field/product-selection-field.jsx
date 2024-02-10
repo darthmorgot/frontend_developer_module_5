27f62d1ad784cd755e259eb99f9081a3cc616302
darthmorgot <dartmorgot@yandex.ru>
2024-02-11 00:47:03 +1000

    Исправил описание, добавил пропсы полю выбора товара.

--- src/components/blocks/order-form/product-selection-field/product-selection-field.jsx
+++ src/components/blocks/order-form/product-selection-field/product-selection-field.jsx
@@ -12,24 +12,27 @@ import {
 
 /**
  * Компонент поля формы для выбора товара из списка на странице каталога
- * @param goods props Данные продукта поступают из OrderForm
- * @param changed props Коллбэк поступает из OrderForm
+ * @param products props Данные продукта поступают из OrderForm
+ * @param selectedValues props Данные о выбранных продуктах поступают из OrderForm
+ * @param setSelectedValues props Коллбэк поступает из OrderForm
+ * @param slideToSelectProduct props Коллбэк поступает из OrderForm
  * @returns {JSX.Element} Разметка поля формы
  * @constructor
  */
-function ProductSelectionField({goods, changed}) {
+function ProductSelectionField({products, selectedValues, setSelectedValues, slideToSelectProduct}) {
   return (
     <SelectFieldset>
       <Title level={TitleLevel.H3} size={TitleSize.SMALL}>Выберите продукты</Title>
       <SelectList>
-        {goods && goods.length ? goods.map(item => (
+        {products && products.length ? products.map((item, index) => (
             <ListItem key={item.id}>
               <Label>
                 <SpanName>{item.title}</SpanName>
                 <Input className='visually-hidden'
                        name={item.name}
-                       checked={item.checked}
-                       onChange={e => changed(item.id, e)}
+                       checked={selectedValues.includes(item.id)}
+                       onChange={() => setSelectedValues(item.id)}
+                       onClick={() => slideToSelectProduct(item.id, index)}
                 />
                 <SpanMark></SpanMark>
               </Label>
