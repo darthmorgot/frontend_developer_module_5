2fbf8e5bec4572b8d5039ab235e846bf62bb2cb9
darthmorgot <dartmorgot@yandex.ru>
2024-02-13 00:10:23 +1000

    Создал новый файл стилизованного компонента для страницы с ошибкой 404.

---
+++ src/components/pages/error404/style.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+import {StyledTitle} from '../../ui/title/style.js';
+
+export const TitleErrorWrapper = styled.div`
+  margin-top: 30px;
+  text-align: center;
+`;
