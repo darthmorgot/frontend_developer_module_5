259e2b65b294469cafb2846f3fdc1f7aaf56b1ab
darthmorgot <dartmorgot@yandex.ru>
2024-02-13 00:08:27 +1000

    Создал новый файл компонента для обнуления скрола при переходе на страницы.

---
+++ src/components/ui/scroll-top/scroll-to-top.jsx
@@ -0,0 +1,19 @@
+import {useEffect} from 'react';
+import {useLocation} from 'react-router-dom';
+
+/**
+ * Компонент для обнуления скрола при переходе на страницу
+ * @returns {null}
+ * @constructor
+ */
+function ScrollToTop() {
+  const {pathname} = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
+export default ScrollToTop;
