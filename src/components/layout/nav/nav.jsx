2179835a8bd4e141e453b9d4002d5efe2fb1a619
darthmorgot <dartmorgot@yandex.ru>
2024-02-13 00:05:31 +1000

    Исправил разметку в связи с применением роутинга, добавил необходимые импорты и массив данных.

--- src/components/layout/nav/nav.jsx
+++ src/components/layout/nav/nav.jsx
@@ -1,21 +1,33 @@
 import React from 'react';
+import {useLocation} from 'react-router-dom';
 import Button from '../../ui/button/button.jsx';
+import {AppRoute} from '../../../AppRoute.js';
 import {StyledNav, Anchor} from './style.js';
+
+const navElements = [
+  {
+    to: AppRoute.MAIN,
+    item: <Button key={AppRoute.BUY} link={AppRoute.BUY}>Купить</Button>
+  },
+  {
+    to: AppRoute.BUY,
+    item: <Anchor key={AppRoute.MAIN} to={AppRoute.MAIN} tabIndex='0'>Главная</Anchor>
+  },
+];
 
 /**
  * Компонент блока навигации в шапке сайте
- * @param page props Данные о странице сайта поступает из Header
- * @param setPage props Коллбэк поступает из Header
  * @returns {JSX.Element} Разметка блока
  * @constructor
  */
-function Nav({page, setPage}) {
+function Nav() {
+  const pageUrl = useLocation().pathname;
   return (
     <StyledNav>
       {
-        page === 'main-page'
-          ? <Button link dataButton={'catalog-page'} handleButton={setPage}>Купить</Button>
-          : <Anchor data-page='main-page' onClick={setPage} tabIndex='0'>Главная</Anchor>
+        navElements
+          .filter(navElement => navElement.to === pageUrl)
+          .map(navElement => navElement.item)
       }
     </StyledNav>
   );
