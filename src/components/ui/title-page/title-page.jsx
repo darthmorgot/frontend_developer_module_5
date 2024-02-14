430174c6c296a9a645419a0d1e2e6f692766c91d
darthmorgot <dartmorgot@yandex.ru>
2024-02-14 23:32:18 +1000

    Мелкие правки кода.

--- src/components/ui/title-page/title-page.jsx
+++ src/components/ui/title-page/title-page.jsx
@@ -3,13 +3,13 @@ import {useEffect} from 'react';
 /**
  * Компонент для установки названия страницы
  * @param namePage props Данные о названии страницы поступают из места применения
- * @returns {null}
+ * @returns {null} Ничего не возвращает
  * @constructor
  */
 function TitlePage({namePage}) {
   useEffect(() => {
     document.title = namePage;
-  }, []);
+  }, [namePage]);
 
   return null;
 }
