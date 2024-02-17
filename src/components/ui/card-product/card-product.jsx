2935c828cae2e8b2188f558ccd864b287c5f8264
darthmorgot <dartmorgot@yandex.ru>
2024-02-18 00:53:42 +1000

    Добавил импорт TitleLevel и TitleSize.

--- src/components/ui/card-product/card-product.jsx
+++ src/components/ui/card-product/card-product.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import Title, {TitleSize, TitleLevel} from '../title/title.jsx';
+import Title from '../title/title.jsx';
+import {TitleLevel, TitleSize} from '../title/title-level-size.js';
 import Tabs from './tabs/tabs.jsx';
 import {Card, Image} from './style.js';
 
