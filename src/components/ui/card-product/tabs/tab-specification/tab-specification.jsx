8606f858246a5d1e0df13cc16981e369dc60ba61
darthmorgot <dartmorgot@yandex.ru>
2024-02-17 22:45:11 +1000

    Мелкие правки кода.

--- src/components/ui/card-product/tabs/tab-specification/tab-specification.jsx
+++ src/components/ui/card-product/tabs/tab-specification/tab-specification.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import OptionList from '../option-list/option-list.jsx';
 
 /**
- * Компонент вкладки с характеристиками товара
- * @param activeTab props Данные об активной вкладке поступают из Tabs
- * @param item props Данные о товаре поступают из Tabs
- * @returns {JSX.Element|null} Разметка вкладки
- * @constructor
+ * Компонент вкладки с характеристиками товара.
+ * @param {string} activeTab Данные (props) об активной вкладке поступают из Tabs.
+ * @param {object} item Данные (props) о товаре поступают из Tabs.
+ * @returns {JSX.Element|null} Разметка вкладки.
+ * @class
  */
 function TabSpecification({activeTab, item}) {
   return activeTab === 'tab_1'
