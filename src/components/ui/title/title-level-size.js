cd1716c89db7f47613b63bd527be9beaddd4f7a1
darthmorgot <dartmorgot@yandex.ru>
2024-02-18 00:55:34 +1000

    Создал новый файл для экспорта констант TitleLevel и TitleSize.

---
+++ src/components/ui/title/title-level-size.js
@@ -0,0 +1,19 @@
+/**
+ * Константа, задающая уровень заголовков.
+ * @type {{H1: string, H2: string, H3: string}}
+ */
+export const TitleLevel = {
+  H1: 'h1',
+  H2: 'h2',
+  H3: 'h3'
+};
+
+/**
+ * Константа, задающая величину шрифта в заголовках.
+ * @type {{BIG: string, SMALL: string, MEDIUM: string}}
+ */
+export const TitleSize = {
+  BIG: 'big',
+  MEDIUM: 'medium',
+  SMALL: 'small'
+};
