4adbf0b3608cf0d6a8833c7d73f52eee59d43b99
darthmorgot <dartmorgot@yandex.ru>
2024-02-17 22:38:33 +1000

    Мелкие правки кода.

--- src/components/ui/advantage-card/advantage-card.jsx
+++ src/components/ui/advantage-card/advantage-card.jsx
@@ -3,14 +3,14 @@ import Title, {TitleSize, TitleLevel} from '../title/title.jsx';
 import {Section, CardHeader, Category, Description} from './style.js';
 
 /**
- * Компонент карточки из списка преимуществ
- * @param category props Данные о категории поступают из AdvantagesList
- * @param title props Данные о названии поступают из AdvantagesList
- * @param description props Данные об описании поступают из AdvantagesList
- * @param icon props Данные об иконке поступают из AdvantagesList
- * @param isNegative props Данные для выбора стиля поступают из AdvantagesList
- * @returns {JSX.Element} Разметка карточки
- * @constructor
+ * Компонент карточки из списка преимуществ.
+ * @param {string} category Данные (props) о категории поступают из AdvantagesList.
+ * @param {string} title Данные (props) о названии поступают из AdvantagesList.
+ * @param {string} description Данные (props) об описании поступают из AdvantagesList.
+ * @param {string} icon Данные (props) об иконке поступают из AdvantagesList.
+ * @param {boolean} isNegative Данные (props) для выбора стиля поступают из AdvantagesList.
+ * @returns {JSX.Element} Разметка карточки.
+ * @class
  */
 function AdvantageCard({category, title, description, icon, isNegative}) {
   return (
