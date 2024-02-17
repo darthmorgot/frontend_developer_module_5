8bca7e12ec7a49444a975b9d28ce3999bece8e70
darthmorgot <dartmorgot@yandex.ru>
2024-02-17 22:44:36 +1000

    Мелкие правки кода.

--- src/components/ui/scroll-top/scroll-to-top.jsx
+++ src/components/ui/scroll-top/scroll-to-top.jsx
@@ -2,9 +2,9 @@ import {useEffect} from 'react';
 import {useLocation} from 'react-router-dom';
 
 /**
- * Компонент для обнуления скрола при переходе на страницу
- * @returns {null} Ничего не возвращает
- * @constructor
+ * Компонент для обнуления скрола при переходе на страницу.
+ * @returns {null} Ничего не возвращает.
+ * @class
  */
 function ScrollToTop() {
   const {pathname} = useLocation();
