c47d90e3078cedf85724beca09b2798dffaf096b
darthmorgot <dartmorgot@yandex.ru>
2024-02-17 22:45:39 +1000

    Мелкие правки кода.

--- src/components/ui/title/title.jsx
+++ src/components/ui/title/title.jsx
@@ -14,12 +14,12 @@ export const TitleSize = {
 };
 
 /**
- * Компонент заголовка
- * @param level props Данные об уровне заголовка поступают на месте применения
- * @param size props Данные о величине шрифта поступают на месте применения
- * @param children props Данные о контенте поступают на месте применения
- * @returns {JSX.Element|null} Разметка заголовка
- * @constructor
+ * Компонент заголовка.
+ * @param {string} level Данные (props) об уровне заголовка поступают на месте применения.
+ * @param {string} size Данные (props) о величине шрифта поступают на месте применения.
+ * @param {string} children Данные (props) о контенте поступают на месте применения.
+ * @returns {JSX.Element|null} Разметка заголовка.
+ * @class
  */
 function Title({level, size, children}) {
   return (
