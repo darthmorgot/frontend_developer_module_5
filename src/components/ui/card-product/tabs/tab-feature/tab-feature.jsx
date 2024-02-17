9ade3f1788380fed697277d9bf9f15afe7f930ea
darthmorgot <dartmorgot@yandex.ru>
2024-02-17 22:45:00 +1000

    Мелкие правки кода.

--- src/components/ui/card-product/tabs/tab-feature/tab-feature.jsx
+++ src/components/ui/card-product/tabs/tab-feature/tab-feature.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import OptionList from '../option-list/option-list.jsx';
 
 /**
- * Компонент блока со свойствами на карточке товара
- * @param activeTab props Данные об активной вкладке поступают из Tabs
- * @param item props Данные о продукте поступают из Tabs
- * @returns {JSX.Element|null} Разметка блока
- * @constructor
+ * Компонент блока со свойствами на карточке товара.
+ * @param {string} activeTab Данные (props) об активной вкладке поступают из Tabs.
+ * @param {object} item Данные (props) о продукте поступают из Tabs.
+ * @returns {JSX.Element|null} Разметка блока.
+ * @class
  */
 function TabFeature({activeTab, item}) {
   return activeTab === 'tab_2'
