5632a2fa5a54f08b4853c0f0749179d4d86e62fd
darthmorgot <dartmorgot@yandex.ru>
2024-02-24 00:12:49 +1000

    Мелкие правки кода.

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {ThemeProvider} from 'styled-components';
-import App from './components/app/app.jsx';
-import {defaultTheme} from './themes/default.js';
+import App from '@/components/app/app.jsx';
+import {defaultTheme} from '@/themes/default.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
