97280e4940a786a00168c37d6ce4043b17e6a060
darthmorgot <dartmorgot@yandex.ru>
2024-02-26 20:26:48 +1000

    Добавил экспорты стилизованных компонентов.

--- src/components/styled/index.js
+++ src/components/styled/index.js
@@ -1,1 +1,5 @@
-export {default as p} from './p/p.js';
+export {default as P} from './p/p.js';
+export {default as Button} from './button/button.js';
+export {default as Img} from './img/img.js';
+export {default as Ul} from './ul/ul.js';
+export {default as Label} from './label/label.js';
