2a6065524a67a73983255bc113cf07eb15a85d12
darthmorgot <dartmorgot@yandex.ru>
2024-02-26 20:26:02 +1000

    Создал новый файл для стилизованного компонента Button.

---
+++ src/components/styled/button/button.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+  color: inherit;
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+`;
+
+export default Button;
