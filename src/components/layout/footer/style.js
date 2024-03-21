bbdcaed7a92e6f07eae18f44074f07527e7a7280
darthmorgot <dartmorgot@yandex.ru>
2024-03-22 00:32:01 +1000

    Мелкие правки стилей.

--- src/components/layout/footer/style.js
+++ src/components/layout/footer/style.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import {P} from '@/components/styled/index.js';
 
 export const StyledFooter = styled.footer`
   border-top: 1px solid ${props => props.theme.blackZeroTen};
@@ -8,6 +9,7 @@ export const FooterWrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  padding-top: 10px;
 `;
 
-export const FooterCopyright = styled.p``;
+export const FooterCopyright = styled(P)``;
